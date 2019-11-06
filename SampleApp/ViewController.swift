01bb9c259d9d1a7492e804f4c5aeba544c6cc0f7
Hiroyuki <mac@makkunomacbook-puro.local>
2019-11-06 10:14:26 +0900

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Hiroyuki on 2019/11/06.
+//  Copyright © 2019年 funkazista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
